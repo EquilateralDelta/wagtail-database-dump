047fd94cf5aae3a69a5b319e6323ca5620100241
Yaroslav Khodakovskij <wwwinche@gmail.com>
2020-06-01 17:57:24 +0300

    Create folder in dump data if it doesn't exist

--- tests/test_dump_data.py
+++ tests/test_dump_data.py
@@ -1,11 +1,30 @@
+import os
+import tempfile
+from unittest import mock
+
 from django.core.management import call_command, CommandError
 from django.test import TestCase
+
+import wagtaildatabasedump
 
 
 class TestDumpDataTestCase(TestCase):
     def test_command_exists(self):
-        try:
-            call_command('wagtaildumpdata')
-        except CommandError as e:
-            if e.args[0].startswith("Unknown command:"):
-                raise
+        with tempfile.TemporaryDirectory() as folder:
+            with mock.patch("wagtaildatabasedump.DEFAULT_FOLDER", folder):
+                try:
+                    self.call_command()
+                except CommandError as e:
+                    if e.args[0].startswith("Unknown command:"):
+                        raise
+
+    def test_creates_default_folder(self):
+        with tempfile.TemporaryDirectory() as folder:
+            os.rmdir(folder)
+            with mock.patch("wagtaildatabasedump.DEFAULT_FOLDER", folder):
+                self.assertFalse(os.path.isdir(folder))
+                self.call_command()
+                self.assertTrue(os.path.isdir(folder))
+
+    def call_command(self, *args, **kwargs):
+        call_command("wagtaildumpdata", *args, **kwargs)
--- wagtaildatabasedump/management/commands/wagtaildumpdata.py
+++ wagtaildatabasedump/management/commands/wagtaildumpdata.py
@@ -1,6 +1,11 @@
+import os
+
 from django.core.management.base import BaseCommand
+import wagtaildatabasedump
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pass
+        folder = wagtaildatabasedump.DEFAULT_FOLDER
+        if not os.path.isdir(folder):
+            os.makedirs(folder)
