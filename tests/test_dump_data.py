c7105b65404baae59008b584bb3c1a12af1e1772
Yaroslav Khodakovskij <wwwinche@gmail.com>
2020-06-01 11:43:40 +0300

    Catch only appropriate exceptions on exist tests

--- tests/test_dump_data.py
+++ tests/test_dump_data.py
@@ -1,7 +1,11 @@
-from django.core.management import call_command
+from django.core.management import call_command, CommandError
 from django.test import TestCase
 
 
 class TestDumpDataTestCase(TestCase):
     def test_command_exists(self):
-        call_command('wagtaildumpdata')
+        try:
+            call_command('wagtaildumpdata')
+        except CommandError as e:
+            if e.args[0].startswith("Unknown command:"):
+                raise
--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -1,7 +1,11 @@
-from django.core.management import call_command
+from django.core.management import call_command, CommandError
 from django.test import TestCase
 
 
 class TestLoadDataTestCase(TestCase):
     def test_command_exists(self):
-        call_command('wagtailloaddata')
+        try:
+            call_command('wagtailloaddata')
+        except CommandError as e:
+            if e.args[0].startswith("Unknown command:"):
+                raise
